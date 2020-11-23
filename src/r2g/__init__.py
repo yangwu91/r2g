d15662f579ef7219bbb49d0bc9435440c658d87a
Yang Wu <yangwu91@outlook.com>
2020-11-23 13:51:53 +0800

    Upgraded to 1.0.5 ready for GUI

--- src/r2g/__init__.py
+++ src/r2g/__init__.py
@@ -14,7 +14,7 @@ __full_name__ = "Reads to Genes"
 __description__ = "A homology-based, computationally lightweight pipeline " \
                   "for discovering genes in the absence of an assembly"
 __url__ = "https://yangwu91.github.io/r2g/"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __author__ = "Yang Wu"
 __maintainer__ = "Yang Wu"
 __author_email__ = "wuyang@drwu.ga"
